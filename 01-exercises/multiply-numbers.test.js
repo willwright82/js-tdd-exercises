a7d263f613919a0299dc17aa7790e41f68ba8b3c
kabaros <kabaros@gmail.com>
2017-06-07 15:35:26 +0100

    Update multiply-numbers.test.js

--- 01-exercises/multiply-numbers.test.js
+++ 01-exercises/multiply-numbers.test.js
@@ -4,7 +4,7 @@
     number of arguments passed to it.
     some expectations:
         multiply(4, 2)  // --> 8
-        multiply(2, 2, 3)  // --> 18
+        multiply(2, 2, 3)  // --> 12
     Write your implementation in TDD
         1. Add a Test (see it failing - RED)
         2. Write Code (to make it pass - GREEN)
